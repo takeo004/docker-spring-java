38d777f812becdc608522c1f2f4e7108ce47a1dd
Tenma Yamazaki <logwhitesesame@gmail.com>
2023-04-18 11:56:02 +0900

    LINEからのリクエストに返信するひな形作成

--- server/src/main/java/com/example/api/ApiApplication.java
+++ server/src/main/java/com/example/api/ApiApplication.java
@@ -1,11 +1,18 @@
 package com.example.api;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
-	public static void main(String[] args) {
+
+	static Path downloadedContentDir;
+	public static void main(String[] args) throws IOException {
+		downloadedContentDir = Files.createTempDirectory("line-bot");
 		SpringApplication.run(ApiApplication.class, args);
 	}
 }
---
+++ server/src/main/java/com/example/api/controller/LineMessageController.java
@@ -0,0 +1,25 @@
+package com.example.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+@LineMessageHandler
+public class LineMessageController {
+    
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
+
+    @EventMapping
+    public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
+        TextMessageContent message = event.getMessage();
+        BotApiResponse response = lineMessagingClient.replyMessage(new ReplyMessage(event.getReplyToken(), new TextMessage("テスト：" + message), true)).get();
+    }
+}
