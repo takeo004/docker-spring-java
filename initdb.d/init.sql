43b7603d6135d28a630d255d9787ad6f2e732c57
Tenma Yamazaki <logwhitesesame@gmail.com>
2023-04-18 14:33:32 +0900

    ダミーユーザー

--- initdb.d/init.sql
+++ initdb.d/init.sql
@@ -1,5 +1,5 @@
 CREATE SCHEMA line_secretary;
 
-CREATE USER admin WITH PASSWORD 'admin@pass@1218';
+CREATE USER username WITH PASSWORD 'userpassword';
 GRANT ALL ON SCHEMA line_secretary TO admin;
 GRANT ALL PRIVILEGES ON DATABASE line_secretary TO admin;
