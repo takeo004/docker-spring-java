91cf677735c12a3461d5a4892c38d0bd16f29372
Tenma Yamazaki <logwhitesesame@gmail.com>
2023-04-20 17:57:37 +0900

    微修正

--- server/src/main/java/com/example/api/entity/UserResponseHistory.java
+++ server/src/main/java/com/example/api/entity/UserResponseHistory.java
@@ -6,8 +6,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Data
 @Table(name = "userResponseHistory")
