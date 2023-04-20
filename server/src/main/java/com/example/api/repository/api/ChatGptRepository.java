88051d3bdd9a57aeab19845ca8437d809c32b9c6
Tenma Yamazaki <logwhitesesame@gmail.com>
2023-04-20 15:55:56 +0900

    レート制限で失敗した場合に5回までリトライするよう修正

--- server/src/main/java/com/example/api/repository/api/ChatGptRepository.java
+++ server/src/main/java/com/example/api/repository/api/ChatGptRepository.java
@@ -6,8 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
+import com.theokanning.openai.OpenAiHttpException;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
-import com.theokanning.openai.completion.chat.ChatCompletionResult;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.service.OpenAiService;
 
@@ -16,6 +16,13 @@ public class ChatGptRepository {
 
     @Value("${chatgpt.token}")
     public String token;
+    @Value("${chatgpt.model}")
+    public String model;
+    @Value("${chatgpt.max-retry}")
+    public int maxRetry;
+    @Value("${chatgpt.retry-sleep-ms}")
+    public long retrySleepMs;
+
 
     public String requestChatGpt(List<ChatMessage> messageList) throws Exception {
         return this.requestChatGpt(messageList, 150, 1d);
@@ -23,18 +30,31 @@ public class ChatGptRepository {
     
     public String requestChatGpt(List<ChatMessage> messageList, int maxTokens, double temperature) throws Exception {
         OpenAiService service = new OpenAiService(token, Duration.ofSeconds(30));
-
         ChatCompletionRequest request = ChatCompletionRequest.builder()
-            .model("gpt-3.5-turbo")
-            .messages(messageList)
-            .n(1)
-            .maxTokens(maxTokens)
-            .temperature(temperature)
-            .stream(false)
-            .build();
-
+        .model(model)
+        .messages(messageList)
+        .n(1)
+        .maxTokens(maxTokens)
+        .temperature(temperature)
+        .stream(false)
+        .build();
+        
+        String response;
+        int retryCount = 0;
+        while(true) {
+            try {
+                response = service.createChatCompletion(request).getChoices().get(0).getMessage().getContent();
+            } catch (OpenAiHttpException e) {
+                if(retryCount == maxRetry) {
+                    response = "エラーが発生しました！繰り返す場合は管理者にご連絡ください！";
+                    break;
+                } else {
+                    Thread.sleep(retrySleepMs);
+                    retryCount++;
+                }
+            }
+        }
         service.shutdownExecutor();
-        ChatCompletionResult result = service.createChatCompletion(request);
-        return result.getChoices().get(0).getMessage().getContent();
+        return response;
     }
 }
